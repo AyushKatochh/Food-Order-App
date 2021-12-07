4e02eeefced91402cc701378344dff9fd1a4cad1
AyushKatochh <katochayushhp@gmail.com>
2021-12-07 19:45:57 +0530

    advanced-react

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react/cjs/react.production.min";
+
+import MealsSummary from "./MealsSummary";
+import AvailableMeals  from "./AvailableMeals";
+
+const Meals = () => {
+    return <Fragment>
+        <MealsSummary />
+        <AvailableMeals />
+    </Fragment>
+};
+
+export default Meals;
